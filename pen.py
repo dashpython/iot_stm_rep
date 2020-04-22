46fcbbe5d890f7f78212da48ac5955e5f91117a6
dashpython <61311476+dashpython@users.noreply.github.com>
2020-04-22 15:13:32 +0530

    Update pen.py

--- pen.py
+++ pen.py
@@ -69,7 +69,8 @@ class DeviceModel(db.Model):
         self.SPA= SPA
         self.TA= TA
 
-
+db.session.add(DeviceModel(stamp= str(datetime.now()+ timedelta(minutes=330)), devId='R1' ,SPA= 51.85, TA=0.0 ))
+db.session.commit()
 def on_connect(client, userdata, flags, rc):
     print("Connected!", rc)
 
